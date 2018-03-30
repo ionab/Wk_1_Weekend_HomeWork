c8e339ff83d69bbf102c969b1633b030afaefc6d
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-30 11:59:59 +0100

    Test 7

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -19,3 +19,7 @@ end
 def increase_pets_sold(pet_shop, add_number_pet_sold)
   return pet_shop[:admin][:pets_sold] += add_number_pet_sold
 end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
--- pet_shop/specs/pet_shop_spec.rb
+++ pet_shop/specs/pet_shop_spec.rb
@@ -71,44 +71,44 @@ class TestPetShop < Minitest::Test
         name: "Camelot of Pets"
       }
   end
-
+#1
   def test_pet_shop_name
     name = pet_shop_name(@pet_shop) #this is an instance variable. Making this available to the test.
     assert_equal("Camelot of Pets", name)
   end
-
+#2
   def test_total_cash
     sum = total_cash(@pet_shop)
     assert_equal(1000, sum)
   end
-
+#3
   def test_add_or_remove_cash__add
     add_or_remove_cash(@pet_shop,10)
     cash = total_cash(@pet_shop)
     assert_equal(1010, cash)
   end
-
+#4
   def test_add_or_remove_cash__remove
     add_or_remove_cash(@pet_shop,-10)
     cash = total_cash(@pet_shop)
     assert_equal(990, cash)
   end
-
+#5
   def test_pets_sold
     sold = pets_sold(@pet_shop)
     assert_equal(0, sold)
   end
-
+#6
   def test_increase_pets_sold
     increase_pets_sold(@pet_shop,2)
     sold = pets_sold(@pet_shop)
     assert_equal(2, sold)
   end
-
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+#7
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
