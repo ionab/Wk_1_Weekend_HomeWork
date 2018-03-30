415be6ce60bcbb7cef3f47faa0ee2d17d5fba6e8
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-30 11:49:35 +0100

    Test 3 and 4

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -1,8 +1,13 @@
+#Don't need to use the @symbol in your arguments in here.
+
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
 
-#Don't need to use the @symbol in your arguments in here.
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  return pet_shop[:admin][:total_cash] += amount
 end
