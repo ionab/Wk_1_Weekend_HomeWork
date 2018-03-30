1f987cd9927392cb7106c84a70c74bedcdc065ec
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-30 11:38:44 +0100

    Test2

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -1,3 +1,8 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+#Don't need to use the @symbol in your arguments in here.
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
