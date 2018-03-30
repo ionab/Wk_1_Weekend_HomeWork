daba567e144f590dca9dc8a7c49146f42b8c7f80
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-30 11:55:51 +0100

    Test 6

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -15,3 +15,7 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop, add_number_pet_sold)
+  return pet_shop[:admin][:pets_sold] += add_number_pet_sold
+end
