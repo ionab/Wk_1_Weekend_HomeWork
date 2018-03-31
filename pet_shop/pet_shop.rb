0cc80f496b675d3d657d9a59f2e5bddb7d1d2c97
Kelsie Braidwood <kelsieiona@gmail.com>
2018-03-31 13:57:05 +0100

    Test 19

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -66,7 +66,59 @@ def add_pet_to_customer(customers, new_pet)
   customers[:pets].push(new_pet)
 end
 
-
 def customer_can_afford_pet(customer, new_pet)
     return customer[:cash] >= new_pet[:price]
+end
+
+
+
+# def test_sell_pet_to_customer__pet_found
+#   customer = @customers[0]
+#   pet = find_pet_by_name(@pet_shop,"Arthur")
+#
+#   sell_pet_to_customer(@pet_shop, pet, customer)
+#
+#   assert_equal(1, customer_pet_count(customer))
+#   assert_equal(1, pets_sold(@pet_shop))
+#   assert_equal(1900, total_cash(@pet_shop))
+# end
+#
+# def test_sell_pet_to_customer__pet_not_found
+#   customer = @customers[0]
+#   pet = find_pet_by_name(@pet_shop,"Dave")
+#
+#   sell_pet_to_customer(@pet_shop, pet, customer)
+#
+#   assert_equal(0, customer_pet_count(customer))
+#   assert_equal(0, pets_sold(@pet_shop))
+#   assert_equal(1000, total_cash(@pet_shop))
+# end
+#
+# def test_sell_pet_to_customer__insufficient_funds
+#   customer = @customers[1]
+#   pet = find_pet_by_name(@pet_shop,"Arthur")
+#
+#   sell_pet_to_customer(@pet_shop, pet, customer)
+#
+#   assert_equal(0, customer_pet_count(customer))
+#   assert_equal(0, pets_sold(@pet_shop))
+#   assert_equal(1000, total_cash(@pet_shop))
+# end
+#
+#
+
+#what happens when you sell a pet... if the customer can afford the pet
+#you add the pet to the customers
+#you deduct the price of a pet from the customers total_cash
+#you remove the pet from the pet shop inventory
+#you increase the number of pets sold
+#you add the cash from the sale to your bank
+def sell_pet_to_customer(pet_shop, pet, customer)
+  if customer_can_afford_pet(customer, pet)
+    add_pet_to_customer(customer, pet)
+    customer[:cash] -= pet[:price]
+    remove_pet_by_name(pet_shop, pet)
+    increase_pets_sold(pet_shop, 1)
+    add_or_remove_cash(pet_shop, pet[:price])
+  end
 end
