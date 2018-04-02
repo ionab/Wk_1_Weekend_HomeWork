a762e9c7b3eaa0ac67c09872e8f2bb25b99cd0e2
Kelsie Braidwood <kelsieiona@gmail.com>
2018-04-02 10:07:07 +0100

    following 121 with comments addressed

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -32,7 +32,6 @@ pets = []
     end
   end
   return pets
-  return 0 #tthis line here will never do anything   - remove
 end
 
 def find_pet_by_name(pet_shop, pet_name)
@@ -70,43 +69,6 @@ def customer_can_afford_pet(customer, new_pet)
     return customer[:cash] >= new_pet[:price]
 end
 
-
-
-# def test_sell_pet_to_customer__pet_found
-#   customer = @customers[0]
-#   pet = find_pet_by_name(@pet_shop,"Arthur")
-#
-#   sell_pet_to_customer(@pet_shop, pet, customer)
-#
-#   assert_equal(1, customer_pet_count(customer))
-#   assert_equal(1, pets_sold(@pet_shop))
-#   assert_equal(1900, total_cash(@pet_shop))
-# end
-#
-# def test_sell_pet_to_customer__pet_not_found
-#   customer = @customers[0]
-#   pet = find_pet_by_name(@pet_shop,"Dave")
-#
-#   sell_pet_to_customer(@pet_shop, pet, customer)
-#
-#   assert_equal(0, customer_pet_count(customer))
-#   assert_equal(0, pets_sold(@pet_shop))
-#   assert_equal(1000, total_cash(@pet_shop))
-# end
-#
-# def test_sell_pet_to_customer__insufficient_funds
-#   customer = @customers[1]
-#   pet = find_pet_by_name(@pet_shop,"Arthur")
-#
-#   sell_pet_to_customer(@pet_shop, pet, customer)
-#
-#   assert_equal(0, customer_pet_count(customer))
-#   assert_equal(0, pets_sold(@pet_shop))
-#   assert_equal(1000, total_cash(@pet_shop))
-# end
-#
-#
-
 #what happens when you sell a pet... if the customer can afford the pet
 #you add the pet to the customers
 #you deduct the price of a pet from the customers total_cash
@@ -117,7 +79,7 @@ end
 #so argument pet needs to not equal nil
 #so update your if statement so pet needs to be true to proceed
 def sell_pet_to_customer(pet_shop, pet, customer)
-  if !pet.nil? && customer_can_afford_pet(customer, pet) #basically enter if true # could refractor this to  if pet != nil 
+  if !pet.nil? && customer_can_afford_pet(customer, pet) #basically enter if true # could refractor this to  if pet != nil
     add_pet_to_customer(customer, pet)
     customer[:cash] -= pet[:price]
     remove_pet_by_name(pet_shop, pet)
